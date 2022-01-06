709375a9cc694ad61f53c07036d13107cec7f73a
Ewerton Maik <ewertonmsa@hotmail.com>
2022-01-06 11:15:58 -0300

    Add files via upload

--- Scripts_R.r
+++ Scripts_R.r
@@ -232,3 +232,116 @@ library(plotrix)
 fatias <- c(4, 12, 14, 16, 8)
 paises <- c("Brasil", "Estados Unidos", "Alemanha", "Reino Unido", "Espanha")
 pie3D(fatias, labels = paises, explode = 0.1, main = "Leitura de Livros Por Pessoa/Ano")
+
+### Line Chart ###
+
+# Dados
+carros     <- c(1, 3, 6, 4, 9)
+caminhoes  <- c(2, 5, 4, 5, 12)
+
+# Plot - Criando Grafico de Linhas
+plot(carros, type = "o", col = "blue", ylim = c(0, 12) ) # Primeira Camada
+
+lines(caminhoes, type = "o", pch = 22, lty = 2, col = "red") # Segunda Camada
+
+title(main = "Produção de Veiculos", col.main = "red", front.main = 4) # Terceira Camada
+
+### Boxplot ###
+library(ggplot2) #Carregando biblioteca em memória
+View(mpg) # Visualizando Conjunto de Dados - dataset
+
+# Plot
+ggplot(mpg, aes(x = reorder(class, hwy), y = hwy, fill = class ) )
++ geom_boxplot()
++ xlab("class")
++ theme(legend.position = "none")
+
+### Scatter Plot ###
+library(ggplot2)
+data = data.frame(cond = rep(c("condition_1", "condition_2"), each = 10 ),
+				  my_x = 1:100 + rnorm(100, sd = 9), my_y = 1:100 + rnorm(100, sd = 16) )
+View(data)
+
+ggplot(data, aes(x = my_x, y = my_y) ) + geom_point(shape = 1) # Gafico de Dispersão
+
+# Adicionando linha de regressão ao Grafico anterior
+ggplot(data, aes(x = my_x, y = my_y) )
++ geom_point(shape = 1)
++ geom_smooth(method = lm, color = "red", se = FALSE)
+
+# Adicionando Smooth
+ggplot(data, aes(x = my_x, y = my_y) )
++ geom_point(shape = 1)
++ geom_smooth(method = lm, color = "red", se = TRUE)
+
+### TreeMap ###
+install.packages("treemap")
+library(treemap)
+
+# Dados
+grupo    = c(rep("grupo-1", 4), rep("grupo-2", 2), rep("grupo-3", 3) )
+subgrupo = paste("subgroup", c(1,2,3,4,1,2,1,2,3), sep = "-" )
+vaor     = c(13, 5, 22, 12, 11, 7, 3, 1, 23)
+dados = data.frame(grupo, subgrupo, valor)
+View(dados)
+
+# Labels
+?treemap
+treemap(dados,
+		index = c("grupo", "subgrupo"),
+		vSize = "valor",
+		type  = "index",
+		fontsize.labels  = c(15, 12),
+		fontcolor.labels = c("white", "orange"),
+		fontface.labels  = c(2, 1),
+		bg.labels = 220,
+		align.labels = list( c("center", "center"), c("right", "bottom") ),
+		overlap.labels = 0.5,
+		inflate.labels = F
+)
+
+# Invertendo e Customizando TreeMap
+treemap(dados,
+		index  = c("grupo", "subgrupo"),
+		vSize  = "valor",
+		type   = "index",
+		border.col  = c("black", "white"),
+		border.lwds = c(7, 2)
+)
+
+### Histogramma ###
+# Gerando valores para X
+x <- mtcars$mpg # Pegando uma única coluna da tabela
+
+# Criando o Histograma
+h <- hist(x,
+		  breaks = 10,
+		  col    = "red",
+		  xlab   = "Milhas Por Galão",
+		  main   = "Histograma com Curva de Distribuição"
+)
+
+# Customizando o Histograma
+xfit <- seq(min(x), max(x), length = 40)
+yfit <- dnorm(xfit, mean = mean(x), sd = sd(x) )
+yfit <- yfit * diff(h$mids[1:2]) * length(x)
+lines(xfit, yfit, col = "blue", lwd = 2)
+
+# Usando o ggplot2
+library(ggplot2)
+
+# Dataset
+dados = data.frame(value = rnorm(10000) )
+View(dados)
+
+# Tamanho das colunas
+ggplot(dados, aes(x = value))
++ geom_histogram(binwidth = 0.05)
+
+# Cor Uniforme
+ggplot(dados, aes(x = value))
++ geom_histogram(binwidth = 0.2, color = "white", fill = rgb(0.2, 0.7, 0.1, 0.4) )
+
+# Cor Proporcional
+ggplot(dados, aes(x = value) )
++ geom_histogram(binwidth = 0.2, aes(fill = ..count..) )
