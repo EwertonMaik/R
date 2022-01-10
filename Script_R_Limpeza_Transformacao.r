b2e784706587f46764110ced575fa6e8edfc6e36
Ewerton Maik <ewertonmsa@hotmail.com>
2022-01-10 20:43:19 -0300

    Add files via upload

--- Script_R_Limpeza_Transformacao.r
+++ Script_R_Limpeza_Transformacao.r
@@ -40,3 +40,16 @@ View(dados_iris_id)
 library(data.table)
 dados_iris_id <- data.table(dados_iris)
 dados_iris_id$Sepal.Length <- as.integer(dados_iris_id$Sepal.Length) # $ valor salvo na propria coluna
+
+# Atividade 3 - Construir um grafico mostrando a relação de duas variaveis numericas para as 3 categorias de uma variavel categorica
+library(ggplot2)
+ggplot(data = dados_iris, aes(x = Petal.Width, y = Petal.Length) )
++ geom_point(aes(color = Species), size = 3 )
++ ggtitle("Largura e Comprimento das Pétalas")
++ labs(x = "Largura da Pétala", y = "Comprimento da Pétala")
++ theme_bw()
++ theme(title = element_text(size = 15, color = "turquoise4") )
+
+# Cores disponiveis na Linguagem R
+colors()
+
