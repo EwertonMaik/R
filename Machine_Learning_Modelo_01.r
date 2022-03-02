f2edd5f22ded924b56f2eb49296096395711efb6
Ewerton Maik <ewertonmsa@hotmail.com>
2022-03-01 23:45:53 -0300

    Add files via upload

--- Machine_Learning_Modelo_01.r
+++ Machine_Learning_Modelo_01.r
@@ -44,3 +44,41 @@ set.seed(9560)
 dados_treino_bal <- SMOTE(inadimplente ~ ., data = dados_treino)
 table(dados_treino_bal$inadimplente)
 prop.table(table(dados_treino_bal$inadimplente) )
+
+# Costruindo a segunda versão do modelo
+modelo_v2 <- randomForest(inadimplente ~ ., data = dados_treino_bal)
+modelo_v2
+
+# Avaliando o Modelo
+plot(modelo_v2)
+
+# Previsoes com dados de Teste
+previsoes_v2 <- predict(modelo_v2, dados_teste)
+
+# Confusion Matrix
+?caret::confusionMatrix
+cm_v2 <- caret::confunsionMatrix(previsoes_v2, dados_teste$inadimplente, positive = "1")
+cm_v2
+
+# Calculando Precision, Recall e F1-Score, metricas de avaliação do modelo preditivo
+y <- dados_teste$inadimplente
+Y_pred_2 <- previsoes_v2
+
+precision <- posPredValue(y_pred_2, y)
+precision
+
+recall <- sensitivity(y_pred_2, y)
+recall
+
+F1 <- (2 * precision * recall) / (precision + recall)
+F1
+
+
+# Importancia das variaveis preditoras para as previsoes
+View(dados_treino_bal)
+varImPlot(modelo_v2)
+
+# Obtendo as variaveis mais importantes
+imp_var <- importance(modelo_v2)
+varImportance <- data.frame(Variables = row.name(imp_var),
+							Importance = round(imp_var[ , 'MeanDecreaseGini'], 2 ))
