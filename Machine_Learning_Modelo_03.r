2cb0201d1bec8a1f34db6c5cfca5b8b17d4aebcb
Ewerton Maik <ewertonmsa@hotmail.com>
2022-03-03 23:24:52 -0300

    Add files via upload

---
+++ Machine_Learning_Modelo_03.r
@@ -0,0 +1,39 @@
+# Modelo 03 de Machine Learning
+
+# Construindo a terceira versão do modelo apenas com variaveis mais importantes
+colnames(dados_treino_bal)
+modelo_v3 <- randomForest(inadimplente ~ PAY_0 + PAY_2 + PAY_3 + PAY_ANT1 + PAY_ANT2 + PAY_5 + BILL_ANT1,
+							data = dados_treino_bal)
+
+modelo_v3
+
+# Avaliando o modelo
+plot(modelo_v3)
+
+# Previsoes com dados de teste
+previsoes_v3 <- predict(modelo_v3, dados_teste)
+
+# Confusion Matrix
+?caret::confusionMatrix
+cm_v3 <- caret::confusionMatrix(previsoes_v3, dados_teste$inadimplente, positive = "1")
+cm_v3
+
+# Calculando Precision, Recall e F1-Score, metricas de avaliacao do modelo preditivo
+y <- dados_teste$inadimplente
+y_pred_v3 <- previsoes_v3
+
+precision <- posPredValue(y_pred_v3, y)
+precision
+
+recall <- sensitivity(y_pred_v3, y)
+recall
+
+F1 <- (2 * precision * recall) / (precision + recall)
+F1
+
+
+# Salvando o modelo da memoria do R Studio em Disco
+saveRDS(modelo_v3, file = "modelo/modelo_v3.rds")
+
+# Carregando o modelo
+modelo_final <- readRDS("modelo/modelo_v3.rds")
