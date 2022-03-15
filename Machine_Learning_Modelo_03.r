d801bf4722a84144d167ea59e0aef1cc9ff71a6d
Ewerton Maik <ewertonmsa@hotmail.com>
2022-03-14 23:28:47 -0300

    Add files via upload

--- Machine_Learning_Modelo_03.r
+++ Machine_Learning_Modelo_03.r
@@ -37,3 +37,37 @@ saveRDS(modelo_v3, file = "modelo/modelo_v3.rds")
 
 # Carregando o modelo
 modelo_final <- readRDS("modelo/modelo_v3.rds")
+
+# Previsoes com novos dados de 3 clientes
+
+# Dados dos clientes
+PAY_0 <- c(0, 0, 0)
+PAY_2 <- c(0, 0, 0)
+PAY_3 <- c(1, 0,0)
+PAY_AMT1 <- c(1100, 1000, 1200)
+PAY_AMT2 <- c(1500, 1300, 1150)
+PAY_5 <- c(0, 0, 0)
+BILL_AMT1 <- c(350, 420, 280)
+
+# Concatena em um dataframe
+novos_clientes <- data.frame(PAY_0, PAY_2, PAY_3, PAY_AMT1, PAY_AMT2, PAY_5, BILL_AMT1)
+View(novos_clientes)
+
+# Previsoes
+previsoes_novo_cliente <- predict(modelo_final, novos_clientes)
+
+
+# Checando os tipos de dados
+str(dados_treino_bal)
+str(novos_clientes)
+
+# Convertendo os tipos de dados
+novos_clientes$PAY_0 <- factor(novos_clientes$PAY_0, levels = levels(dados_treino_bal$PAY_0))
+novos_clientes$PAY_0 <- factor(novos_clientes$PAY_2, levels = levels(dados_treino_bal$PAY_2))
+novos_clientes$PAY_0 <- factor(novos_clientes$PAY_3, levels = levels(dados_treino_bal$PAY_3))
+novos_clientes$PAY_0 <- factor(novos_clientes$PAY_5, levels = levels(dados_treino_bal$PAY_5))
+str(novos_clientes)
+
+# Previsoes
+previsoes_novos_clientes <- predict(modelo_final, novos_clientes)
+View(previsoes_novo_cliente)
